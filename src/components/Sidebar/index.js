80141bfee876f29834a2d6221acf7deb90721200
Vishal Joshi <vishalj026@gmail.com>
2022-03-31 11:27:21 +0530

    Added Sidebar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,47 @@
+import { faUser, faHome,faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import {faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link, NavLink } from 'react-router-dom';
+
+import LogoS from '../../assets/images/logo-s.png';
+import LogoSubtitle from '../../assets/images/logo_sub.png';
+import './index.scss';
+
+const Sidebar = () => {
+    return (
+        <div className="nav-bar">
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt="logo" />
+            <img className='sub-logo' src={LogoSubtitle} alt="slobodan" />
+        </Link>
+
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e"></FontAwesomeIcon>
+            </NavLink>
+
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e"></FontAwesomeIcon>
+            </NavLink>
+
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"></FontAwesomeIcon>
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel="noopener noreferer" href="https://www.linkedin.com/in/vishal-joshi-207716106">
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"></FontAwesomeIcon>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel="noopener noreferer" href="https://github.com/vishalj026">
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e"></FontAwesomeIcon>
+                </a>
+            </li>
+        </ul>
+        </div>
+    );
+}
+
+export default Sidebar;
